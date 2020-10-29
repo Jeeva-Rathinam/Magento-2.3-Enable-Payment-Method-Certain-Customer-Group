ea381b64c0469b303ff599d58cbcfc1d0772c1e9
Jeeva Rathinam <jeevarathinamit@gmail.com>
2020-10-29 12:55:27 +0530

    Add website filter

--- app/code/Gta/EnablePaymentMethod/Observer/PaymentMethodEnable.php
+++ app/code/Gta/EnablePaymentMethod/Observer/PaymentMethodEnable.php
@@ -2,26 +2,26 @@
 namespace Gta\EnablePaymentMethod\Observer;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+
 class PaymentMethodEnable implements ObserverInterface {
+  
     protected $_customerSession;
+    protected $_storeManager;
+
     public function __construct(
-       \Magento\Customer\Model\Session $customerSession
+       \Magento\Customer\Model\Session $customerSession,
+       \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
        $this->_customerSession = $customerSession;
+       $this->_storeManager = $storeManager;
     }
-    // public function execute(Observer $observer) {
-    //    $payment_method_code = $observer->getEvent()->getMethodInstance()->getCode();
-    //    if ($payment_method_code == 'paypal_express') {
-    //        $result = $observer->getEvent()->getResult();
-    //        if ($this->_customerSession->isLoggedIn()) {
-    //            $customerGroupId = $this->_customerSession->getCustomer()->getGroupId();
-    //            if ($customerGroupId == 9) {
-    //                $result->setData('is_available', true);
-    //            }
-    //        }
-    //    }
-    // }
+
     public function execute(Observer $observer) {
+
+      $website_id = $this->_storeManager->getStore()->getWebsiteId();
+      if($website_id == '2')
+      {
+
       $payment_method_code = $observer->getEvent()->getMethodInstance()->getCode();
       if ($payment_method_code == 'paypal_express') {
         $result = $observer->getEvent()->getResult();
@@ -33,5 +33,6 @@ class PaymentMethodEnable implements ObserverInterface {
             }
         }
       }
+    }
     }
 }
